db3118b92f838d574a5feffdbabbde9ca980dd73
Jirka Neumann <jirka.neumann@gmx.de>
2025-02-24 23:59:34 +0100

    update script

--- line_breaks.R
+++ line_breaks.R
@@ -28,3 +28,22 @@ for (i in 1:2){
 
   cat(paste0("\nmax_length: ", max_length, "\n-----------\n", result_string, "\n\n"))
 }
+
+#-------------- try hyphenation + + + first steps --------------#
+
+
+library(sylly)
+library(sylly.en)
+#library(sylly.de) #--> not available for R-Version 4.4.2
+
+sampleText = c("This", "is", "a", "rather", "stupid", "demonstration")
+Beispieltxt = c("Lange", "Wörter", "Beispiel", "Burgturm", "Haustür", "Schlüsselloch", "Schlauchboot")
+
+
+hyph.txt.en <- hyphen(sampleText, hyph.pattern="en")
+#hyph.txt.en <- hyphen(Beispieltxt, hyph.pattern="de")
+
+print(hyph.txt.en)
+
+
+
