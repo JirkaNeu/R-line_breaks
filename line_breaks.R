565b94585cbd13fcf62d284aecf4fe300f433a95
Jirka Neumann <jirka.neumann@gmx.de>
2025-04-11 22:47:23 +0200

    update

--- line_breaks.R
+++ line_breaks.R
@@ -35,12 +35,11 @@ library(sylly)
 library(sylly.en)
 library(sylly.de) #--> https://undocumeantit.github.io/repos/l10n/pckg/sylly.de/index.html
 
-sampleText = c("This", "is", "a", "rather", "stupid", "demonstration")
-Beispieltxt = c("Lange", "Wörter", "Beispiel", "Burgturm", "Haustür", "Schlüsselloch", "Schlauchboot")
+#sampleText = c("This", "is", "a", "rather", "stupid", "demonstration")
+#Beispieltxt = c("Lange", "Wörter", "Beispiel", "Burgturm", "Haustür", "Schlüsselloch", "Schlauchboot")
 #print(hyph.txt.en = hyphen(sampleText, hyph.pattern="en"))
 #print(hyph.txt.de = hyphen(Beispieltxt, hyph.pattern="de"))
-
-print(hyph.txt.de[6]$word)
+#print(hyph.txt.de[6]$word)
 
 breakword = function(getword, lang = "en"){
   if (lang == "en"){
@@ -48,14 +47,46 @@ breakword = function(getword, lang = "en"){
   else{
     brword = hyphen(getword, hyph.pattern="de")}
   brword = brword[1]$word
-  brword = (sub("-", "_", brword, fixed = T))
-  brword = (gsub("-", "", brword, fixed = T))
-  brword = (sub("_", "-\n", brword, fixed = T))  
+  if (grepl("-", brword) == T){
+    brword = (sub("-", "_", brword, fixed = T))
+    brword = (gsub("-", "", brword, fixed = T))
+    brword = (sub("_", "-\n", brword, fixed = T))
+  }else{
+    brword = getword
+  }
   return(brword)
 }
 
 cat(breakword("Schlauchboot", "de"))
+cat(breakword("heute", "de"))
+cat(breakword("Uhr", "de"))
 
-
+#-- again
+#--------------------
+for (i in 1:2){
+  max_length = round(max_length/i)
+  all_length = nchar(my_string)
+  big_word = max(nchar(strsplit(my_string, " ")[[1]]))
+  x_test = big_word
+  x_start = 1
+  space_last = 0
+  result_string = c()
+  
+  if (all_length > max_length & max_length > big_word){
+    repeat{
+      x_start = x_start + space_last
+      x_end = x_start + max_length
+      teilstring = substr(my_string, x_start, x_end)
+      if(x_end > all_length) {Teilsatz = teilstring; result_string = c(result_string, Teilsatz); break} #--> stopp when end of my_string
+      space_last = max(which(strsplit(teilstring, "")[[1]]==" ")) #--> last blank in $teilstring
+      Teilsatz = substr(teilstring, 1, space_last - 1) #--> last blank out
+      result_string = c(result_string, Teilsatz)
+      if (x_test > all_length){break}; x_test = x_test + 1 #--> security system
+    }
+    result_string = paste(result_string, collapse = "\n")
+  }else result_string = paste0("- value to small, nothing changed -\n", my_string)
+  
+  cat(paste0("\nmax_length: ", max_length, "\n-----------\n", result_string, "\n\n"))
+}
 
 
